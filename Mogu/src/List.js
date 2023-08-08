f5e655ad0be3751bef446ab89f1d0062a2cdfa79
bizzze <bizzee27@gmail.com>
2023-08-08 18:15:41 +0900

    Docs: 상세페이지 수정

--- Mogu/src/List.js
+++ Mogu/src/List.js
@@ -18,16 +18,17 @@ function List(props) {
         </div>
         <div className="sub_container">
           <div className="sub_img">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxOTEyMjlfMjMg/MDAxNTc3NjIwOTU2Nzc2.4EPT2eWeEaJjb43TFSJPMsDESmJmnELAF6MsF-R9IjIg.xXvk2PZcuyBZAxYGnHhLkx0s2yduK3GZihi-i1Yj3Wwg.JPEG.yellowouk2/1577620956351.jpg?type=w800"></img>
+            <img src="https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTVfNTkg/MDAxNTc5MDUxMTM2MzYy.1WTLuTdfI6-9BOmoInzfzwZKJCT-ZA-TS5fmK73Ncgcg.SE37huyBMMTHha6z3bErwlxCV5nHAaJFAHQ87QNyrV8g.JPEG.yellowouk2/1579051135206.jpg?type=w800"></img>
           </div>
           <div className="sub_info">
             <div className="sub_title">
-              <h1>다다일상(오셜록)</h1>
+              <span>다다일상(오셜록)</span>
             </div>
             <div className="sub_summary">
-              <span>
-                다채롭고 다양한 일상을 위한 오셜록만의 특별한 차 구독 서비스
-              </span>
+              <span>다채롭고 다양한 일상을 위한</span>
+            </div>
+            <div className="sub_summary">
+              <span>오셜록만의 특별한 차 구독 서비스</span>
             </div>
           </div>
         </div>
@@ -74,7 +75,23 @@ function List(props) {
             </ul>
           </nav>
           <div className="sub_detail_container">
-            {showInfoTab && <div className="tab">정보에용</div>}
+            {showInfoTab && (
+              <div className="tab">
+                <div className="tab_sub">
+                  ‘예스어스’는 자연 그대로 자란 “못난이 농산물”을 “저렴하게”
+                  받아볼 수 있는 농산물 정기 배송 서비스입니다
+                </div>
+                <div className="tab_sub">
+                  버려질 위기에 처한 친환경 농산물을 예스어스를 통해 소비할 수
+                  있습니다 예스어스에 들어가는 농산물들은 자연 그대로 자랐기에
+                  크기와 생김새가 다양합니다
+                </div>
+                <div className="tab_sub">
+                  화학 비료, 성장 촉진제, 왁스, 호르몬제를 사용하지 않는 친환경
+                  농산물로 안심하고 드실 수 있습니다
+                </div>
+              </div>
+            )}
             {showSubscriptionTab && (
               <div className="tab">구독 방법 탭이에용</div>
             )}
